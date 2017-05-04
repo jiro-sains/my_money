55a75d4bc13bb8be546206fbf06e66e5e2363aa1
Jacqueline Rosains <jrosains@gmail.com>
2017-05-04 20:56:36 +0000

    session with Piot

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
   #require 'calculations'
   def index
+    #binding.pry
+    @total_expenses = Calculations::Expense.total
+    @avg_expenses = Calculations::Expense.promedio
+    @max_expenses = Calculations::Expense.max
   end
+
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,15 +1,3 @@
 class Expense < ApplicationRecord
 
-  def self.total
-  	sum(:amount)
-  end
-
-  def self.promedio
-  	average(:amount)
-  end
-
-  def self.max
-  	maximum(:amount)
-  end
-
 end
